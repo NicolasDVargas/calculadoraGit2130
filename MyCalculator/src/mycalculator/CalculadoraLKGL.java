c6bb7ee6c532a10704042261c5086af9c75737b3
Lida Guerrero <tuchysguer@hotmail.com>
2021-08-10 09:56:00 -0500

    clase calculadora lida

---
+++ MyCalculator/src/mycalculator/CalculadoraLKGL.java
@@ -0,0 +1,35 @@
+package mycalculator;
+
+public class CalculadoraLKGL implements ICalculator{
+
+	@Override
+	public double add(double a, double b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public double multiply(double a, double b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	@Override
+	public double divide(double a, double b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+	@Override
+	public double substract(double a, double b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public String about() {
+		// TODO Auto-generated method stub
+		return "Calculator implementend by "+Authors.LKGL;
+	}
+
+}
