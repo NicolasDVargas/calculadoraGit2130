2b38bccadf1ee202f2143d26147e546a0885851e
AndresGarciaMutis <78189982+AndresGarciaMutis@users.noreply.github.com>
2021-08-11 10:44:55 -0500

    Create CalculadoraAMGM.java

---
+++ MyCalculator/src/mycalculator/CalculadoraAMGM.java
@@ -0,0 +1,30 @@
+package mycalculator;
+
+public class CalculadoraAMGM implements ICalculator {
+
+	@Override
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	@Override
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+
+	@Override
+	public double divide(double a, double b) {
+		return a / b;
+	}
+
+	@Override
+	public double substract(double a, double b) {
+		return a - b;
+	}
+
+	@Override
+	public String about() {
+		return "Calculadora implementada por " + Authors.AMGM;
+	}
+
+}
